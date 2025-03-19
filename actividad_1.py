35a5d5a346e62f27a6f62b4adebcbe85826bbceb
Steven <stev508ga@gmail.com>
2025-03-19 09:36:48 -0500

    Actividad entrada por consola

--- actividad_1.py
+++ actividad_1.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 temperatura = int(input ("Ingrese la temperatura del motor: "))
-=======
-temperatura = int(input ("Ingrese la temperatura del motor: ")) 
->>>>>>> if_else
 
 if temperatura > 80:
     print("Motor Apagado")
